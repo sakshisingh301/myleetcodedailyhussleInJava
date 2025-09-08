2de21f9e6f4d883c78d640e94718b22bf6f3b471
sakshisingh301 <sakshisingh03aug.ss@gmail.com>
2025-09-08 10:55:57 -0700

    daily questions

---
+++ Revise/ValidPalindromeII.java
@@ -0,0 +1,41 @@
+public class ValidPalindromeII {
+
+    public boolean validPalindrome(String s) {
+
+        int start=0;
+        int end=s.length()-1;
+
+        while(start<end)
+        {
+
+            if(s.charAt(start)!=s.charAt(end))
+            {
+                return isPalindrome(start+1,end,s)|| isPalindrome(start,end-1,s);
+            }
+            start++;
+            end--;
+        }
+        return true;
+
+    }
+
+    private boolean isPalindrome(int start, int end, String s) {
+
+        while (start<end)
+        {
+            if(s.charAt(start)!=s.charAt(end))
+            {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        String s = "abca";
+
+    }
+}
